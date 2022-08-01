aa7dd77bcf3cccd815c17bafe8b1295ba527ee2f
Farid Hidayat <farid.hidayat@sagara.id>
2022-08-01 18:22:35 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,10 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| salt laravel employee attendance Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register routes for your package.
+|
+*/
